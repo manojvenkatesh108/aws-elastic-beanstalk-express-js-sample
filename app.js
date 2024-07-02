fb975ee54fa953b933c91b0ac4f30069bc8452fb
manojvenkatesh108 <manojv@checkpoint.com>
2024-07-02 16:01:19 +1000

    minor change 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! I am online'));
+app.get('/', (req, res) => res.send('Hello World! This is Manu and I am online'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
